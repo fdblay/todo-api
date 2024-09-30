6ade89bc748da3f2789c8d8e8f3e318d0c1e7972
FrederickKumah <fdblay@gmail.com>
2024-09-30 15:53:46 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// bringing in codes
+import express from 'express';
+
+// create an express app
+const app = express();
+
+//  Define routes
+app.get('/hello', function (req, res, next) {
+    console.log(req.headers);
+    res.json('You visited the hello endpoint!');
+});
+
+app.get('/goodbye', function (req, res, next) {
+    console.log(req.query);
+    res.json('Same to you');
+});
+
+//  Listen for incoming requests
+app.listen(3000, function () {
+    console.log('App is listening on port 3000')
+});
