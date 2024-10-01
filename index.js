a1002b86ad484a7423176e9ba254577542f5a9f2
FrederickKumah <fdblay@gmail.com>
2024-10-01 13:54:56 +0000

    created routes and controller folders

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 // bringing in codes
 import express from 'express';
+import todoRouter from './routes/todo.js';
 
 // create an express app
 const app = express();
 
-//  Define routes
-app.get('/hello', function (req, res, next) {
-    console.log(req.headers);
-    res.json('You visited the hello endpoint!');
-});
-
-app.get('/goodbye', function (req, res, next) {
-    console.log(req.query);
-    res.json('Same to you');
-});
+//  Use routes
+app.use(todoRouter);
 
 //  Listen for incoming requests
-app.listen(3000, function () {
+app.listen(3000, () => {
     console.log('App is listening on port 3000')
 });
